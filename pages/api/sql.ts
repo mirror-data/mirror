57b67f962a7241f5db899a1cb566f6c06d2ba205
Kevin Xu <canwenxu@126.com>
2022-10-21 18:44:48 -0700

    Refactor

--- pages/api/sql.ts
+++ pages/api/sql.ts
@@ -1,33 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import { connection } from '../../utils/db';
+import {connection} from "../../utils/db";
 
 export interface SQLResponse {
-  rows?: string[][]
-  columns?: string[]
-  error?: string
+    rows: string[][]
+    columns: string[]
 
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<SQLResponse>
+    req: NextApiRequest,
+    res: NextApiResponse<SQLResponse>
 ) {
 
-  connection.query(
-    req.body.sql,
-    (err, rawResults) => {
-      const results = rawResults as string[][]
-      if (err) {
-        res.status(200).json({error: `${err.message}`})
-        return
-      }
-      if (results.length > 0) {
-        const columns = Object.keys(results[0])
-        const rows = results.map((row) => Object.values(row))
-        res.status(200).json({columns, rows})
-      }
-      res.status(200).json({columns: [], rows: []})
-    }
-  );
+    connection.query(
+        req.body.sql,
+        (err, results, fields) => {
+            if (err) {
+                res.status(200).json({error: `${err}`})
+                return
+            }
+            if (results.length > 0) {
+                const columns = Object.keys(results[0])
+                const rows = results.map((row) => Object.values(row))
+                res.status(200).json({columns, rows})
+            }
+            res.status(200).json({columns: [], rows: []})
+        }
+    );
 }
