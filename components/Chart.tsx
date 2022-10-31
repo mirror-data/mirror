6975dc8234159a53d6c07b0fb2115c4b7baf830c
xuliwenwenwen <liwen.xu@pingcap.com>
2022-10-31 10:41:10 +0800

    feat: add dark theme
    Signed-off-by: xuliwenwenwen <liwen.xu@pingcap.com>

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,17 +1,17 @@
-import {Vega} from "react-vega";
-import {Handler} from 'vega-tooltip';
-import dynamic from "next/dynamic";
-import React from "react";
+import { Vega } from 'react-vega'
+import { Handler } from 'vega-tooltip'
+import dynamic from 'next/dynamic'
+import React from 'react'
 
 interface ChartProps {
   config: any
 }
 
-const Chart: React.FC<ChartProps> = ({config}) => {
-  return <Vega spec={config} tooltip={new Handler().call}/>
+const Chart: React.FC<ChartProps> = ({ config }) => {
+  return <Vega spec={config} theme="dark" tooltip={new Handler().call} />
 }
 const NoSSR = dynamic(() => Promise.resolve(Chart), {
-  ssr: false
+  ssr: false,
 })
 
 export default NoSSR
