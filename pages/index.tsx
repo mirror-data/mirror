76762d744c518fa524d274706d85a68b6cca9d04
wph95 <justwph@gmail.com>
2022-10-31 12:20:00 -0700

    add html head

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ import {
   summaryStatusAtom
 } from "@/components/v2/state";
 import {fetchSQL} from "@/components/v2/apis";
+import Head from "next/head";
 
 interface Repo {
   value: string
@@ -57,6 +58,11 @@ export default () => {
     backgroundColor: "rgb(237 242 255 / 40%)",
     backgroundImage: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23E7F5FF'%3E%3Cpath d='M0 .5H31.5V32'/%3E%3C/svg%3E\")",
   }} className="h-screen w-screen p-4 flex flex-col">
+    <Head>
+      <title>Mirror for OSSInsight</title>
+      <link rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪞</text></svg>"/>
+    </Head>
     <div className="flex gap-2 p-4 bg-white pb-6 rounded-lg justify-between items-center">
       <div className="flex gap-2 flex-grow ">
         <Autocomplete
